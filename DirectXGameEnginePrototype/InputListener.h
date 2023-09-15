b6b81e3d648452c813808ff7e0ab8e87333b8b08
Julia Sabelli <61601709+JSabelli-FDX@users.noreply.github.com>
2023-09-15 05:19:30 -0400

    Quick Fix

--- DirectXGameEnginePrototype/InputListener.h
+++ DirectXGameEnginePrototype/InputListener.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Point.h"
 
 
 
@@ -12,7 +12,16 @@ public:
 
 	}
 
-	// KEYBOARD Pure virtual callback functions.
+	// KEYBOARD pure virtual callback functions.
 	virtual void onKeyDown(int key) = 0;
 	virtual void onKeyUp(int key) = 0;
+
+	// MOUSE pure virtual callback functions.
+	virtual void onMouseMove(const Point& delta_mouse_pos) = 0;
+
+	virtual void onLeftMouseDown(const Point& mouse_pos) = 0;
+	virtual void onLeftMouseUp(const Point& mouse_pos) = 0;
+
+	virtual void onRightMouseDown(const Point& mouse_pos) = 0;
+	virtual void onRightMouseUp(const Point& mouse_pos) = 0;
 };
