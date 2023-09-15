d4782134bf26599a98481415a376912092abcc6d
Julia Sabelli <61601709+JSabelli-FDX@users.noreply.github.com>
2023-09-15 03:55:05 -0400

    Revert "Merge branch 'master' of https://github.com/JSabelli-FDX/DirectXGameEnginePrototype"
    This reverts commit 98ca46d2badb9dc975784b3b41254695662827b2, reversing
    changes made to 630c87e5f560bcfe8afd69e0f6e6161d2f48965c.

--- DirectXGameEnginePrototype/InputSystem.h
+++ DirectXGameEnginePrototype/InputSystem.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "InputListener.h"
-#include "Point.h"
-#include <unordered_set>
+#include <map>
 
 class InputSystem
 {
@@ -16,13 +15,8 @@ public:
 public:
 	static InputSystem* get();
 private:
-	std::unordered_set<InputListener*> m_set_listeners;
-
+	std::map<InputListener*, InputListener*> m_map_listeners;
 	unsigned char m_keys_state[256] = {};
 	unsigned char m_old_keys_state[256] = {};
-
-	Point m_old_mouse_pos;
-
-	bool m_first_time = true;
 };
 
